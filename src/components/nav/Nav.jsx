bff3a5014a69bc6246f4bf2409bffd64b107eb11
Yusupov Elbek <elbeky829@gmail.com>
2023-09-21 20:50:01 +0500

    nav(5%) added

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Nav.scss'
+import '../../components/normalize/Normalize.scss'
+
+const Nav = () => {
+  return (
+    <nav>
+        <div className="container">
+            <div className="nav__wrapper">
+                <div className="logo"><img src="" alt="" /></div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
